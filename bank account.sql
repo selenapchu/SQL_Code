d55d0c60b7d2e58d95b2d819baba439914f7e8f9
Selena Chu <119372913+selenapchu@users.noreply.github.com>
2023-03-24 15:08:30 -0700

    Add files via upload
    commit

--- bank account.sql
+++ bank account.sql
@@ -1,12 +1,12 @@
---Find all CD accounts with balance above 1000
---and all CHK accounts with balance above 2000 dollars
+/*Find all CD accounts with balance above 1000
+And all CHK accounts with balance above 2000 dollars*/
 SELECT ACCOUNT_ID, AVAIL_BALANCE, OPEN_DATE
 FROM ACCOUNT
 WHERE PRODUCT_CD LIKE 'CD' AND AVAIL_BALANCE > 1000
 or PRODUCT_CD LIKE 'CHK' AND AVAIL_BALANCE > 2000
 ;
 
---How many checking accounts opened at branch 2 with balance above 1000?
+/*How many checking accounts opened at branch 2 with balance above 1000?*/
 SELECT COUNT(*)
 FROM ACCOUNT
 WHERE OPEN_BRANCH_ID = 2
@@ -14,14 +14,14 @@ AND avail_balance > 1000
 AND product_cd LIKE 'CHK'
 ;
 
---Join BRANCH and PRODUCT tables 
+/*Join BRANCH and PRODUCT tables*/
 SELECT A.ACCOUNT_ID, P.NAME AS PRODUCT_NAME, P.PRODUCT_TYPE_CD, B.NAME AS BRANCH_NAME
 FROM BRANCH B JOIN (ACCOUNT A JOIN PRODUCT P
 ON A.PRODUCT_CD = P.PRODUCT_CD)
 ON B.BRANCH_ID = A.OPEN_BRANCH_ID
 ;
 
---Join BRANCH, ACCOUNT, and INDIVIDUAL tables
+/*Join BRANCH, ACCOUNT, and INDIVIDUAL tables*/
 SELECT A.ACCOUNT_ID, I.FNAME, I.LNAME, B.NAME AS Name_of_Branch, B.STATE
 FROM BRANCH B JOIN (ACCOUNT A LEFT JOIN INDIVIDUAL I
 ON A.CUST_ID = I.CUST_ID)
@@ -29,7 +29,7 @@ ON B.BRANCH_ID = A.OPEN_BRANCH_ID
 ORDER BY A.ACCOUNT_ID
 ;
 
---Join E<PLOYEE and ACCOUNT tables and find out MAX/MIN of available balance
+/*Join EMPLOYEE and ACCOUNT tables and find out MAX/MIN of available balance*/
 SELECT A.PRODUCT_CD, MAX(A.AVAIL_BALANCE), MIN(A.AVAIL_BALANCE), COUNT(A.ACCOUNT_ID), E.LNAME
 FROM EMPLOYEE E JOIN ACCOUNT A
 ON A.OPEN_EMP_ID = E.EMP_ID
@@ -37,14 +37,14 @@ GROUP BY A.PRODUCT_CD, E.LNAME
 ORDER BY COUNT(A.ACCOUNT_ID) DESC
 ;
 
---Use 'LIKE' and 'NOT LIKE' syntax to filter out teller works in 2001, 2002, and 2004
+/*Use 'LIKE' and 'NOT LIKE' syntax to find the teller works in 2001, 2002, and 2004*/
 SELECT LNAME, DEPT_ID, SUPERIOR_EMP_ID, START_DATE, TITLE
 FROM EMPLOYEE
 WHERE TITLE LIKE '%Teller' AND START_DATE NOT LIKE '%03' AND START_DATE NOT LIKE '%00'
 ORDER BY START_DATE DESC
 ;
 
---Join BRANCH, DEPARTMENT, and EMPLYEE tables to find out who is managing others
+/*Join BRANCH, DEPARTMENT, and EMPLYEE tables to find out who is managing others*/
 SELECT DISTINCT E.emp_id, E.fname, E.lname, D.NAME, B.NAME
 FROM branch B JOIN
         (
@@ -59,7 +59,7 @@ FROM branch B JOIN
 WHERE emp_id IN (SELECT superior_emp_id FROM employee)
 ;
 
---Use 'COUNT' to show how many managers there are
+/*Use 'COUNT' to aggregate the number of managers*/
 SELECT COUNT(DISTINCT E.emp_id) AS emp_count
 FROM branch B JOIN
         (
@@ -74,7 +74,7 @@ FROM branch B JOIN
 WHERE emp_id IN (SELECT superior_emp_id FROM employee)
 ;
 
---Right outer join
+/*Use RIGHT OUTER JOIN to combine ACCOUNT and EMPLOYEE tables and ensure all accounts are counted*/
 SELECT M.lname, 
 A.open_emp_id, 
 SUM(A.avail_balance) AS total_balance, 
@@ -85,14 +85,14 @@ LEFT OUTER JOIN employee M ON E.superior_emp_id = M.emp_id
 GROUP BY M.lname, A.open_emp_id
 ;
 
---Create a view with checking accounts with balance above 1000
+/*Create a VIEW with checking accounts with balance above 1000*/
 CREATE VIEW V AS
 SELECT account_id, product_cd, avail_balance
 FROM ACCOUNT
 WHERE avail_balance > 1000
 ;
 
---Subquery
+/*Use subquery*/
 SELECT account_id, avail_balance
 FROM ACCOUNT
 WHERE product_cd LIKE 'CHK'
